ab2f6b55fa5caa14465c6c2c0d5892789b026805
Hieu Luu <luutrunghieucva@gmail.com>
2018-09-16 09:47:24 +0700

    fix responsive

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,44 @@
 import React, { Component } from "react";
-import { Row, Col } from 'antd';
-import {BrowserRouter,Link, Route} from 'react-router-dom';
-import NewTab from './NewTab';
-import CommentTab from './CommentTab';
+// import { Row, Col } from 'antd';
+import { BrowserRouter, Link, Route } from "react-router-dom";
+// import { Container, Row, Col } from "reactstrap";
+
+import NewTab from "./NewTab";
+import CommentTab from "./CommentTab";
+
 import "./App.css";
 class App extends Component {
   state = {
-    navItems: [
-        'new','comments','show','ask','jobs','submit'
-    ]
-  }
+    navItems: ["new", "comments", "show", "ask", "jobs", "submit"]
+  };
   render() {
     return (
       <BrowserRouter>
-      <div className="App">
-        <Row className="container" style={{ margin: "0 auto", width: "80%" }}>
-          <Col span={1}>
-            <Link to="/" className="logo">
-              Y
-            </Link>
-          </Col>
-          <Col span={3}>
-            <Link to="/news" className="title">
-              Hacker News
-            </Link>
-          </Col>
-          <Col span={17}>
-            {this.state.navItems.map(item => (
-              <Link to={ `/${item}`} className="tab">
-                {item}
-              </Link>
-            ))}
-          </Col>
-          <Col span={3}>
-            <Link to="" className="login">
-              log in
-            </Link>
-          </Col>
-        </Row>
-        <Route path='/'/>
-        <Route path='/new' component={NewTab}/>
-        <Route path='/comments' component={CommentTab}/>
-      </div>
+        <div>
+          <nav>
+            <ul className="container">
+              <li className="logo">
+                <Link to="/">Y</Link>
+              </li>
+              <li className="title">
+                <Link to="/">Hacker News</Link>
+              </li>
+              <ul className="tabs">
+                {this.state.navItems.map(item => (
+                  <li>
+                    <Link to={`/${item}`} className="tab">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+              <li className="login">login</li>
+            </ul>
+          </nav>
+          <Route exact path="/" />
+          <Route path="/new" component={NewTab} />
+          <Route path="/comments" component={CommentTab} />
+        </div>
       </BrowserRouter>
     );
   }
